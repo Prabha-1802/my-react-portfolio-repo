0b1b757b8030bf6562aba64346c4172a8e519178
Prabha-1802 <banuprabha6464@gmail.com>
2025-03-29 11:30:20 +0530

    pushed history.json path successfully

--- src/Components/Expierence/Expierence.jsx
+++ src/Components/Expierence/Expierence.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import styles from "./Exp.module.css";
 import skills from '../../data/skills.json';
-import history from '../../data/history.json';
+import history from '../../data/History.json';
 
 
 export const Expierence = () => {
